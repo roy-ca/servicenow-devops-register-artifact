dff5748b4ae0dc0e388dcd8b182a7bd1147e2e1b
roy-ca <roy.ca@servicenow.com>
2023-05-03 19:16:03 +0530

    Adding error cases

--- src/main.js
+++ src/main.js
@@ -78,8 +78,11 @@ const axios = require('axios');
             httpHeaders = { headers: defaultHeadersForBasicAuth };
             snowResponse = await axios.post(endpoint, JSON.stringify(payload), httpHeaders);
         }
+        else if(username === '' || password === '') {
+            core.setFailed("For Basic Auth, Username and Password is mandatory for integration user authentication");
+        }
         else {
-            core.setFailed('Please provide either token or user name password and try again');
+            core.setFailed('Either secret token or integration username, password is needed for integration user authentication');
         }
     } catch (e) {
         if (e.message.includes('ECONNREFUSED') || e.message.includes('ENOTFOUND') || e.message.includes('405')) {
